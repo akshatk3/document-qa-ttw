a5bd50815e65cabda751e3994f6fa5581b2bb45a
akshatk3 <ak.akshatk@gmail.com>
2025-01-16 15:36:19 -0500

    trial run

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,57 @@
 import streamlit as st
 from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_openai import ChatOpenAI 
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+st.title("📄 QA Doc Mbbs ttw")
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+client = OpenAI(api_key=openai_api_key)
+llm_model = "gpt-4o" 
+llm = ChatOpenAI(temperature = 0.7, model=llm_model)
+
+
+uploaded_file = st.file_uploader(
+    "Upload a document", type=("txt", "pdf", "etc.")
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
+#embed it and perform similarity search etc
+user_text = st.text_input("What you want huh?: ")
 
-    if uploaded_file and question:
+if uploaded_file and user_text:
 
-        # Process the uploaded file and question.
+    # Process the uploaded file and question.
         document = uploaded_file.read().decode()
+        vector_store=InMemoryVectorStore.from_documents(document, OpenAIEmbeddings())
+        retriever = vector_store.as_retriever()
+        
+        results = vector_store.similarity_search(user_text, retriever=retriever, top_k=5)
+        
+        r = results[0].page_content
+        
+        # Long text displayed as separate lines
+        long_text_lines = r.splitlines()
+
+        # Scrollable container
+        with st.container():
+            for line in long_text_lines:
+                st.write(line)
+
+        
+        
         messages = [
             {
                 "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+                "content": f"Here's the source document: {results[0].cast_id_to_str} \n\n---\n\n {user_text}",
             }
         ]
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model=llm_model,
             messages=messages,
             stream=True,
         )
